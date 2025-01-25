dc1827103f8114df478fcf504d2b68e97909163f
Hirotaka-0926 <abehirotaka94@gmail.com>
2025-01-26 01:23:13 +0900

    とりあえずページ

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="item center text-3xl text-bold">自己紹介サイト</h1>
+      <p className="">私は会津大学に通っています</p>
+      <p>私の趣味</p>
+      <ul>
+        <li>ボードゲーム</li>
+        <li>ビデオゲーム</li>
+        <li>スキー</li>
+        <li>おかしづくり</li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
