db10c653021382213a3e4e35405ba9dbf0c3a53e
Hirotaka-0926 <abehirotaka94@gmail.com>
2025-01-26 14:50:54 +0900

    station 3

---
+++ server/index.js
@@ -0,0 +1,13 @@
+/* server.ts */
+
+import express from "express";
+
+const app = express();
+const port = 9000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("port : " + port);
+});
